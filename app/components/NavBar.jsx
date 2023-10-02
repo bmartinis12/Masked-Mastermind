c04f15e221aaa899696225da3fac6266885a33ac
Benjamin Martinis <martinisbenjamin@gmail.com>
2023-10-02 15:41:55 -0500

    Update NavBar.jsx

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Link from "next/link";
 import Logo from '../../public/maskedLogo.png';
 
 export default function NavBar() {
   return (
     <nav>
-        <Image src={Logo} alt="Masked Masterming Logo" width={150} quality={100} placeholder="blur" />
+        <h2>Mastermind Network</h2>
+        <Image className="logo" src={Logo} alt="Masked Masterming Logo" width={100} quality={100} placeholder="blur" />
     </nav>
   )
 }
