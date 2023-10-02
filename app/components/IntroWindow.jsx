6d3b0d1664b28572eee489a8636a8f66a97fa7a9
Benjamin Martinis <martinisbenjamin@gmail.com>
2023-10-01 22:55:12 -0500

    Update IntroWindow.jsx

--- app/components/IntroWindow.jsx
+++ app/components/IntroWindow.jsx
@@ -3,7 +3,7 @@
 export default function IntroWindow() {
   return (
     <div className="intro-container">
-        <video className="bg-video" autoPlay loop muted >
+        <video className="bg-video" poster="/videoPoster.png" playsInline autoPlay loop muted >
           <source src="/stocks.mp4" type="video/mp4" />
         </video>
         <div className="intro">
